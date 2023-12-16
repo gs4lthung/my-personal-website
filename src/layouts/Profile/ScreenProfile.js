2a0267f8f0ebf8b8e16430b3cb0122b075a03d57
Lâm Tiên Hưng <Hungltse170216@fpt.edu.vn>
2023-12-16 16:23:59 +0700

    add LazyLoadImage

--- src/layouts/Profile/ScreenProfile.js
+++ src/layouts/Profile/ScreenProfile.js
@@ -1,11 +1,18 @@
 import React from "react";
-
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import "./ScreenProfile.scss";
 import myavt from "../../assets/images/my_avt.png";
+
 export default function ScreenProfile() {
   return (
     <div className="form-layout">
-      <img className="form-layout__img" src={myavt} alt="LTHung" />
+      <LazyLoadImage
+        className="form-layout__img"
+        alt="LTHung"
+        effect="blur" // Apply the blur effect while loading
+        src={myavt}
+      />
     </div>
   );
 }
